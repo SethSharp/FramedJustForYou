650748253c4328471e6b35ece1f4f2dea7f690b6
Seth Sharp <sesharp@outlook.com>
2023-09-11 21:15:03 +1000

    remove welcome, add tw to app, remove some breeze stuff

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>{{ config('app.name', 'Framed Just 4 You') }}</title>
+
+        @vite('resources/css/app.css')
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
