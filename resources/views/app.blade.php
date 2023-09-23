97400812f911d23007bf763d340d2e6ab7b2f8c5
Seth Sharp <sesharp@outlook.com>
2023-09-23 13:44:38 +1000

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,8 +10,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        @vite('resources/css/app.css')
-
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
