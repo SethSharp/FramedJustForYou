99f0d34cadc0b2e55efd5f0ed14893e3ea1eb970
Seth Sharp <sesharp@outlook.com>
2023-09-23 13:24:48 +1000

    deployment config files

--- resources/js/Components/Button/SecondaryButton.vue
+++ resources/js/Components/Button/SecondaryButton.vue
@@ -9,7 +9,7 @@ defineProps({
     <Link
         :href="href"
         class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold
-                text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none
+                text-xs text-gray-700 tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none
                 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-outduration-150"
     >
         <slot />
