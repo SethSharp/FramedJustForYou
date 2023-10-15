00b49d24ae2d82883effbdad048410eb33f10ec7
Seth Sharp <sesharp@outlook.com>
2023-10-15 19:41:58 +1000

    wip

--- resources/js/Pages/Services/Memorabilia.vue
+++ resources/js/Pages/Services/Memorabilia.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
 import PrimaryLayout from "@/Layouts/PrimaryLayout.vue";
+import Section from "@/Components/Section.vue";
 </script>
 
 <template>
     <Head title="Memorabilia" />
 
     <PrimaryLayout>
-        Memorabilia
+        <Section>
+            <template #header> Memorabilia </template>
+            <template #subtitle> Cherishing your memories in art work </template>
+            <template #content>
+                Aliqua tempor irure fugiat non aute officia Lorem nulla esse esse. Do labore ipsum dolore ullamco magna nostrud sit elit amet. Id id ut id voluptate laborum culpa veniam eiusmod. Magna sint minim voluptate labore ex anim qui ad nulla sit eiusmod. Anim excepteur cupidatat amet anim ipsum occaecat est cupidatat consequat elit nulla fugiat qui adipisicing.
+            </template>
+        </Section>
     </PrimaryLayout>
 </template>
