b4c760a28ce62a4e182cbec8554b417f3091f58b
Seth Sharp <sesharp@outlook.com>
2023-10-21 15:33:56 +1000

    re-add the store

--- resources/js/Pages/Store.vue
+++ resources/js/Pages/Store.vue
@@ -2,23 +2,15 @@
 import { Head } from '@inertiajs/vue3';
 import PrimaryLayout from "@/Layouts/PrimaryLayout.vue";
 import Section from "@/Components/Section.vue";
-import {
-    BuildingStorefrontIcon,
-} from "@heroicons/vue/20/solid/index.js";
-import OpeningHours from "@/Components/OpeningHours.vue";
-import ContactDetails from "@/Components/ContactDetails.vue";
 </script>
 
 <template>
-    <Head title="Find us" />
+    <Head title="Store" />
 
     <PrimaryLayout>
         <Section>
             <template #header>
                 Coming soon...
-            </template>
-            <template #content>
-                Online store for Framed Just For You...
             </template>
         </Section>
     </PrimaryLayout>
