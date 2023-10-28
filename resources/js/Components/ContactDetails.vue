d4193f93796d1a7c3f43e6fedb226f65138d7eb9
Seth Sharp <sesharp@outlook.com>
2023-10-28 15:46:12 +1000

    open mail or tel on click contact details

--- resources/js/Components/ContactDetails.vue
+++ resources/js/Components/ContactDetails.vue
@@ -7,19 +7,19 @@ import { DevicePhoneMobileIcon, PhoneIcon, EnvelopeIcon } from '@heroicons/vue/2
         <div class="my-2">
             <div class="flex gap-x-2">
                 <DevicePhoneMobileIcon class="w-8 h-8" />
-                <span> 0412 068 330 </span>
+                <a href="tel:0412068330" class="font-bold"> 0412 068 330 </a>
             </div>
         </div>
         <div class="my-2">
             <div class="flex gap-x-2">
                 <PhoneIcon class="w-8 h-8" />
-                <span> 07 5563 9799 </span>
+                <a href="tel:0755639799" class="font-bold"> 07 5563 9799 </a>
             </div>
         </div>
         <div class="my-2">
             <div class="flex gap-x-2">
                 <EnvelopeIcon class="w-8 h-8" />
-                <span class="my-auto text-sm md:text-lg"> framedjust4u@bigpond.com </span>
+                <a href="mailto:framedjust4u@gmail.com" class="my-auto text-sm md:text-lg font-bold"> framedjust4u@bigpond.com </a>
             </div>
         </div>
     </div>
