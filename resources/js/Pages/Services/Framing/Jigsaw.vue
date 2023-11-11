4d30472c8ba7d5b215573b3e8967276111312cce
Seth Sharp <sesharp@outlook.com>
2023-11-11 11:06:39 +1000

    jig saw steps

--- resources/js/Pages/Services/Framing/Jigsaw.vue
+++ resources/js/Pages/Services/Framing/Jigsaw.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { Head } from '@inertiajs/vue3'
 import Section from '@/Components/Section.vue'
-import Carousel from '@/Components/Carousel.vue'
-import SplideContent from '@/Components/Splide/SplideContent.vue'
 import PrimaryLayout from '@/Layouts/PrimaryLayout.vue'
 import Image from '@/Components/Image.vue'
 </script>
@@ -14,50 +12,94 @@ import Image from '@/Components/Image.vue'
         <Section>
             <template #header> Jigsaw Puzzles </template>
             <template #content>
-                <Carousel type="fade" :autoPlay="false">
-                    <SplideContent
-                        image="jigsaw/giraffe.png"
-                        title="Enjoy Forever"
-                        styles="w-1/2 my-auto"
-                        content="Jigsaw puzzles are fun, relaxing and provide enjoyment for the whole family. Once complete, there is no better way to preserve and display your jigsaw puzzle than by having it professionally framed, allowing you to hang it on your wall like an artwork."
-                    />
-                    <SplideContent
-                        image="framing/matboards.png"
-                        title=""
-                        content="With over 1000 picture frames to choose from you will have no shortage of choice when picking the perfect frame for your jigsaw puzzle. Here at Framed Just For You we take a conservation framing approach to all of our work and our process for jigsaw puzzle framing is no exception."
-                    />
-                </Carousel>
+                    <div class="sm:flex mt-8">
+                        <div class="sm:w-1/2 sm:w-full my-auto">
+                            We have a specialised printer in store, where we can print just about
+                            anything in the highest quality. As you can see in the image, we have the
+                            ability to print very large images and keep the quality of that image in
+                            tact. Allowing us to just about do photo on printing paper to go perfectly
+                            as a background to your frame.
+                        </div>
+                        <div class="sm:w-1/2 sm:w-full">
+                            <Image src="jigsaw/giraffe.png" class="w-1/2 mx-auto" />
+                        </div>
+                    </div>
+                <p class="my-6">
+                    At Framed Just For You, you'll find a vast selection of over 1000 picture frames,
+                    ensuring you have abundant options to find the ideal frame for your jigsaw puzzle.
+                    Our commitment to conservation framing is evident in every aspect of our work, and
+                    this dedication extends to our meticulous process for framing jigsaw puzzles.
+                </p>
             </template>
         </Section>
 
         <Section>
             <template #header> The Process </template>
             <template #content>
+
                 The process we take is very smooth and efficient to ensure your jigsaw is not
-                damaged or messed up and make it stand out within your home.
-                <div class="my-6">
-                    <Carousel type="fade" :autoPlay="false">
-                        <SplideContent
-                            image="jigsaw/stage-1.png"
-                            title="The Process"
-                            content="Each step of our jigsaw puzzle framing is completed in house using the best materials and equipment, guaranteeing it is finished to the highest standards. Your jigsaw puzzle is adhered to the backboard using a pH neutral dry mount acid free adhesive. Then we apply an artshield uv heatseal overlay which is put in our heat press at 97 degrees."
-                        />
-                        <SplideContent
-                            image="jigsaw/stage-2.png"
-                            title="Selecting a frame"
-                            content=""
-                        />
-                        <SplideContent
-                            image="jigsaw/stage-2.png"
-                            title="Backing"
-                            content="Keeps the jigsaw together."
-                        />
-                        <SplideContent
-                            image="jigsaw/stage-2.png"
-                            title="The Results"
-                            content="The result of this process ensures your framed jigsaw puzzle is fade resistant, preserved and easily maintained by simply wiping clean."
-                        />
-                    </Carousel>
+                damaged or messed up and make it stand out within your home. Each step of our jigsaw puzzle
+                framing is completed in house using the best materials and equipment, guaranteeing it is finished
+                to the highest standards.
+
+                <div class="mt-8">
+                    <div class="sm:flex my-4">
+                        <div class="sm:w-1/2 sm:w-full my-auto">
+                            <Image
+                                src="jigsaw/initial-stage.png"
+                            />
+                        </div>
+                        <div class="sm:w-1/2 sm:w-full p-6">
+                            <h1 class="text-xl font-bold">First Steps</h1>
+                            <p>
+                                First step is to acquire a jigsaw and completing it. This can be any jigsaw you currently own or from the store for 10 bucks, we can make any jigsaw look like a million dollars!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="sm:flex my-4">
+                        <div class="sm:w-1/2 sm:w-full p-6">
+                            <h1 class="text-xl font-bold">First Steps</h1>
+                            <p>
+                                Bring the complete jigsaw in, this is where we will help you decide on the right frame that best suits the jigsaw. In this case we have chosen a double frame, with a nice blue colour to accent the ocean waves a thicker frame to match the colour scheme.
+                            </p>
+                        </div>
+                        <div class="sm:w-1/2 sm:w-full my-auto">
+                            <Image
+                                src="jigsaw/frame.png"
+                                class="w-1/2 mx-auto"
+                            />
+                        </div>
+                    </div>
+                    <div class="sm:flex my-4">
+                        <div class="w-full p-6">
+                            <h1 class="text-xl font-bold">Backing</h1>
+                            <p>
+                                The rest is up to us now! We apply our own backing to keep the jigsaw in place, it is adhered to the backboard using a pH neutral dry mount acid free adhesive.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="sm:flex my-4">
+                        <div class="w-full p-6">
+                            <h1 class="text-xl font-bold">Overlay</h1>
+                            <p>
+                                The overlay is important to preserve the jigsaw and allow you to clean it. We apply an artshield uv heatseal overlay which is put in our heat press at 97 degrees. This process also prevents the jigsaw from fading!
+                            </p>
+                        </div>
+                    </div>
+                    <div class="sm:flex my-4">
+                        <div class="sm:w-1/2 sm:w-full my-auto">
+                            <Image
+                                src="jigsaw/final-product.png"
+                                class="w-1/2 mx-auto"
+                            />
+                        </div>
+                        <div class="sm:w-1/2 sm:w-full p-6">
+                            <h1 class="text-xl font-bold">Final Product</h1>
+                            <p>
+                                The result of this process ensures your framed jigsaw puzzle is fade resistant, preserved and easily maintained by simply wiping clean.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </template>
         </Section>
--- resources/js/Pages/Services/Other/ReadyMade.vue
+++ resources/js/Pages/Services/Other/ReadyMade.vue
@@ -12,8 +12,10 @@ import PrimaryLayout from '@/Layouts/PrimaryLayout.vue'
             <template #header> Ready Made Frames </template>
             <template #subtitle> Ready to hang </template>
             <template #content>
-                // canvas ready to paint, offset frame - take picture -> canvases already stretched
+                canvas ready to paint, offset frame - take picture -> canvases already stretched
                 ready to paint
+                We can get your print that matches one of our ready made frames, take it out back and put it together with the frame and a piece of your chosen glass
+                perfect for a cheaper alternativ for your common sized pieces yet still produces an amazing result
             </template>
         </Section>
     </PrimaryLayout>
