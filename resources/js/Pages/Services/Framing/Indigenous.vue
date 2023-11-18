b4443fcfb8b0453d5a0159d0e96dfc3b91913832
Seth Sharp <sesharp@outlook.com>
2023-11-18 13:10:23 +1000

    prettier

--- resources/js/Pages/Services/Framing/Indigenous.vue
+++ resources/js/Pages/Services/Framing/Indigenous.vue
@@ -17,12 +17,12 @@ import Image from '@/Components/Image.vue'
                         Immerse yourself in the rich cultural tapestry of Indigenous Australian art,
                         a captivating expression of the world's oldest living culture. Drawing
                         inspiration from the ancient Dreamtime stories and the diverse landscapes of
-                        the continent. For every piece we find its perfect match, a frame meticulously chosen to
-                        enhance its beauty. Visit us to see how our love for framing transforms
-                        pictures into timeless masterpieces.
+                        the continent. For every piece we find its perfect match, a frame
+                        meticulously chosen to enhance its beauty. Visit us to see how our love for
+                        framing transforms pictures into timeless masterpieces.
                     </div>
                     <div class="w-1/2 my-2 sm:w-2/5 mx-auto">
-                        <Image src="Indigenous/indigenous.webp" alt="Indigenous print"/>
+                        <Image src="Indigenous/indigenous.webp" alt="Indigenous print" />
                     </div>
                 </div>
             </template>
