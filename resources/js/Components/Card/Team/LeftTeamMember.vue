2623ef5c8e373889eb399f87a3d6f8c1cc4de3f9
Seth Sharp <sesharp@outlook.com>
2023-11-18 12:05:31 +1000

    prettier

--- resources/js/Components/Card/Team/LeftTeamMember.vue
+++ resources/js/Components/Card/Team/LeftTeamMember.vue
@@ -11,13 +11,8 @@ const props = defineProps({
 </script>
 
 <template>
-    <div
-        class="w-full flex-wrap md:flex-no-wrap flex my-6 rounded-xl overflow-hidden"
-    >
-        <div
-            :class="{ 'lg:w-full': !src, 'lg:w-1/2': src }"
-            class="w-full flex"
-        >
+    <div class="w-full flex-wrap md:flex-no-wrap flex my-6 rounded-xl overflow-hidden">
+        <div :class="{ 'lg:w-full': !src, 'lg:w-1/2': src }" class="w-full flex">
             <div class="mx-8 my-10 rounded-l-xl transition duration-700 p-2">
                 <h1 class="text-4xl text-gray-600">{{ name }}</h1>
                 <span class="text-2xl text-gray-500"> {{ position }} </span>
@@ -26,15 +21,8 @@ const props = defineProps({
                 </p>
             </div>
         </div>
-        <div
-            class="w-full lg:w-1/2 flex"
-        >
-            <Image
-                v-if="src"
-                class="mx-auto my-auto w-3/5"
-                :src="src"
-                :alt="alt"
-            />
+        <div class="w-full lg:w-1/2 flex">
+            <Image v-if="src" class="mx-auto my-auto w-3/5" :src="src" :alt="alt" />
         </div>
     </div>
 </template>
--- resources/js/Components/Card/Team/RightTeamMember.vue
+++ resources/js/Components/Card/Team/RightTeamMember.vue
@@ -12,20 +12,10 @@ const props = defineProps({
 
 <template>
     <div class="w-full flex-wrap md:flex-no-wrap flex my-6 rounded-xl overflow-hidden">
-        <div
-            class="w-full lg:w-1/2 flex"
-        >
-            <Image
-                v-if="src"
-                class="mx-auto my-auto w-3/5"
-                :src="src"
-                :alt="alt"
-            />
+        <div class="w-full lg:w-1/2 flex">
+            <Image v-if="src" class="mx-auto my-auto w-3/5" :src="src" :alt="alt" />
         </div>
-        <div
-            :class="{ 'lg:w-full': !src, 'lg:w-1/2': src }"
-            class="w-full flex"
-        >
+        <div :class="{ 'lg:w-full': !src, 'lg:w-1/2': src }" class="w-full flex">
             <div class="mx-8 my-10 rounded-r-xl transition duration-700 p-2">
                 <h1 class="text-4xl text-gray-600">{{ name }}</h1>
                 <span class="text-2xl text-gray-500"> {{ position }} </span>
