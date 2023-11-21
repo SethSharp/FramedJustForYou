afd813b6cb79ccc0f10aa53c54f46e400fc3fcca
Seth Sharp <sesharp@outlook.com>
2023-11-21 22:11:52 +1000

    test vite over https

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        https: true
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
