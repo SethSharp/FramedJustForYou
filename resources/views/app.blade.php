f71d7e3d8faad927245e5b1160a98d39d95a77d8
Seth Sharp <sesharp@outlook.com>
2023-11-21 21:11:47 +1000

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,22 +5,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title> Framed Just For You - Custom Picture Framing - Gold Coast </title>
+        <title inertia>{{ config('app.name', 'Framed Just For You') }}</title>
+        @inertiaHead
+
         <meta name="keywords" content="FramedJustForYou, Framed Just For you, custom frames, picture framing, Gold Coast framing, framing services">
         <meta name="description" content="Framed Just For You is Gold Coasts best picture framing business. We have been locally owned and operated for over 20 years and with 60+ years of experience producing amazing results for our clients all over the Gold Coast and the country.">
 
         <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
 
-        <title inertia>{{ config('app.name', 'Framed Just For You') }}</title>
+        @vite('resources/js/app.js')
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
         @routes
-        @vite('resources/js/app.js')
-        @inertiaHead
     </head>
     <body class="font-sans antialiased">
         @inertia
