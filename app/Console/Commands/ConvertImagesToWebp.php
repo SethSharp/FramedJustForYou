90bb275af60dfd26126cf34756cced22a06aa1f2
Seth Sharp <sesharp@outlook.com>
2023-12-03 06:22:55 +1000

    dry-run

--- app/Console/Commands/ConvertImagesToWebp.php
+++ app/Console/Commands/ConvertImagesToWebp.php
@@ -8,24 +8,26 @@ use Illuminate\Support\Facades\Storage;
 
 class ConvertImagesToWebp extends Command
 {
-    protected $signature = 'convert';
+    protected $signature = 'convert {--dry-run}';
     protected $description = 'Convert images from png to webp';
 
     public function handle(): void
     {
-        $folderPath = 'images';
+        $folderPath = 'images/framing';
         $files = Storage::disk('s3')->files($folderPath);
 
         foreach ($files as $file) {
             $imageContent = Storage::disk('s3')->get($file);
 
-            $img = Image::make($imageContent)
-                ->encode('webp');
-            
-            $convertedPath = $folderPath . '/converted/' . pathinfo($file, PATHINFO_FILENAME) . '.webp';
-            Storage::disk('s3')->put($convertedPath, $img, 'public');
+            if ($this->option('dry-run')) {
+                $this->info('Image: ' . $file);
+            } else {
+                $img = Image::make($imageContent)
+                    ->encode('webp');
 
-            break;
+                $convertedPath = $folderPath . '/converted/' . pathinfo($file, PATHINFO_FILENAME) . '.webp';
+                Storage::disk('s3')->put($convertedPath, $img, 'public');
+            }
         }
 
         $this->info('Image processing complete.');
