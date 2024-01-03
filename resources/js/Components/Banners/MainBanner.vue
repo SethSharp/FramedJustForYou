ba032fb857d3d2eec8d81f0c59e4fb47585dcb9c
Seth Sharp <sesharp@outlook.com>
2024-01-03 10:46:00 +1000

    wip

--- resources/js/Components/Banners/MainBanner.vue
+++ resources/js/Components/Banners/MainBanner.vue
@@ -1,7 +1,3 @@
-<script setup>
-import { Link } from '@inertiajs/vue3'
-</script>
-
 <template>
     <div class="bg-primary-500 text-white text-center justify-center py-1">
         Welcome to 2024! Check out our
