4982276503ddec4053dba019a1429298e654c5d2
Seth Sharp <sesharp@outlook.com>
2024-02-13 19:58:08 +1000

    wip

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,18 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>
-        @yield('title', config('app.name'))
+        Testing me!
     </title>
 
     <meta name="keywords"
           content="Gold Coast Picture Framing, Framed Just For you, custom picture framing, Gold Coast, Framing Services, Queensland Framing">
     <meta name="description"
           content="Framed Just For You is Gold Coasts best picture framing business. We have been locally owned and operated for over 20 years and with 60+ years of experience we produce amazing results for our customers all over the Gold Coast and the country!">
-
-    <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon.ico') }}">
-
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
     @vite('resources/js/public.js')
 
