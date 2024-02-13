cd3c5fba79a0c10ca3e3c806a498331b26201ea9
Seth Sharp <sesharp@outlook.com>
2024-02-13 19:48:53 +1000

    actually define public in service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -35,6 +35,9 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('public')
+                ->group(base_path('routes/public.php'));
         });
     }
 }
