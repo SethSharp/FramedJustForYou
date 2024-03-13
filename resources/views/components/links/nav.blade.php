15feba4f305640b74ecd347bdcbc01ef162f5c26
Seth Sharp <sesharp@outlook.com>
2024-03-14 07:16:59 +1000

    wip mega

--- resources/views/components/links/nav.blade.php
+++ resources/views/components/links/nav.blade.php
@@ -1,5 +1,7 @@
 <a
-    href="{{$href}}" class="text-white flex text-2xl md:text-xl items-center decoration-6 underline-offset-4 transition duration-300 ease-in-out {{ $active ? '!underline hover:!underline-none' : '' }}"
+    {{ $attributes->merge(['class' => 'text-white flex text-2xl md:text-xl items-center decoration-6 underline-offset-4 transition duration-300 ease-in-out ']) }}
+    href="{{$href}}"
+    class="{{ $active ? '!underline hover:!underline-none' : '' }}"
 >
     <x-links.underline> {{ $slot }}</x-links.underline>
 </a>
--- resources/views/components/links/services-mega-menu.blade.php
+++ resources/views/components/links/services-mega-menu.blade.php
@@ -1,3 +1,92 @@
+@php
+    $customLinks_1 = [
+        [
+            'href' => route('services.framing.acrylic'),
+            'name' => 'Acrylic',
+            'active' => request()->route()->getName() === 'services.framing.acrylic'
+        ],
+        [
+            'href' => route('services.framing.australian'),
+            'name' => 'Australian Made Framing Kits',
+            'active' => request()->route()->getName() === 'services.framing.australian'
+        ],
+        [
+            'href' => route('services.framing.canvases'),
+            'name' => 'Canvases',
+            'active' => request()->route()->getName() === 'services.framing.canvases'
+        ],
+        [
+            'href' => route('services.framing.indigenous-art'),
+            'name' => 'Indigenous Art',
+            'active' => request()->route()->getName() === 'services.framing.indigenous-art'
+        ],
+        [
+            'href' => route('services.framing.jigsaws'),
+            'name' => 'Jigsaws',
+            'active' => request()->route()->getName() === 'services.framing.jigsaws'
+        ],
+    ];
+
+    $customLinks_2 = [
+        [
+            'href' => route('services.framing.medals-memorabilia'),
+            'name' => 'Medals and Memorabilia',
+            'active' => request()->route()->getName() === 'services.framing.medals-memorabilia'
+        ],
+        [
+            'href' => route('services.framing.mirror'),
+            'name' => 'Mirror',
+            'active' => request()->route()->getName() === 'services.framing.mirror'
+        ],
+        [
+            'href' => route('services.framing.original-artwork'),
+            'name' => 'Original Artwork',
+            'active' => request()->route()->getName() === 'services.framing.original-artwork'
+        ],
+        [
+            'href' => route('services.framing.prints-posters-and-photos'),
+            'name' => 'Prints, Posters, and Photos',
+            'active' => request()->route()->getName() === 'services.framing.prints-posters-and-photos'
+        ],
+        [
+            'href' => route('services.framing.hand-crafted'),
+            'name' => 'Handcrafted',
+            'active' => request()->route()->getName() === 'services.framing.hand-crafted'
+        ],
+    ];
+
+    $printingLinks = [
+        [
+            'href' => route('services.printing'),
+            'name' => 'Printing',
+            'active' => request()->route()->getName() === 'services.printing'
+        ]
+    ];
+
+    $otherLinks = [
+        [
+            'href' => route('services.other.glass-cutting'),
+            'name' => 'Glass Cutting',
+            'active' => request()->route()->getName() === 'services.other.glass-cutting'
+        ],
+        [
+            'href' => route('services.other.matboard-cutting'),
+            'name' => 'Matboard Cutting',
+            'active' => request()->route()->getName() === 'services.other.matboard-cutting'
+        ],
+        [
+            'href' => route('services.other.ready-made'),
+            'name' => 'Ready Made',
+            'active' => request()->route()->getName() === 'services.other.ready-made'
+        ],
+        [
+            'href' => route('services.other.framing-supplies'),
+            'name' => 'Framing Supplies',
+            'active' => request()->route()->getName() === 'services.other.framing-supplies'
+        ]
+    ];
+@endphp
+
 <div
     x-data="{open: false}"
 >
@@ -16,11 +105,47 @@
     <div class="inset-0 absolute z-50 sm:flex" x-show="open" x-on:click="open = false">
         <div class="mt-16 w-4/5 mx-auto shadow-2xl">
             <div
-                class="bg-secondary-200 rounded-xl shadow-xl px-4 mx-auto mt-2 py-6 md:flex hidden flex-col sm:flex-row max-w-7xl w-full shadow-lg"
+                class="bg-secondary-50 rounded-xl shadow-xl px-4 mx-auto mt-2 py-6 md:flex hidden max-w-7xl w-full"
             >
                 <div class="flex w-full">
                     <div class="grid grid-cols-4 gap-y-4 w-full border-gray-200">
-                        Columns here
+                        <div>
+                            <h3> Custom Framing </h3>
+
+                            @foreach ($customLinks_1 as $link)
+                                <x-links.nav class="!text-black !text-md" href="{{$link['href']}}" active="{{$link['active']}}">
+                                    {{ $link['name'] }}
+                                </x-links.nav>
+                            @endforeach
+                        </div>
+
+                        <div>
+                            @foreach ($customLinks_2 as $link)
+                                <x-links.nav class="!text-black !text-md" href="{{$link['href']}}" active="{{$link['active']}}">
+                                    {{ $link['name'] }}
+                                </x-links.nav>
+                            @endforeach
+                        </div>
+
+                        <div>
+                            <h3> Printing </h3>
+
+                            @foreach ($printingLinks as $link)
+                                <x-links.nav class="!text-black !text-md" href="{{$link['href']}}" active="{{$link['active']}}">
+                                    {{ $link['name'] }}
+                                </x-links.nav>
+                            @endforeach
+                        </div>
+
+                        <div>
+                            <h3> Other </h3>
+
+                            @foreach ($otherLinks as $link)
+                                <x-links.nav class="!text-black !text-md" href="{{$link['href']}}" active="{{$link['active']}}">
+                                    {{ $link['name'] }}
+                                </x-links.nav>
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
