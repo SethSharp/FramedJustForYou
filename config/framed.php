73590e875263146b44319b0a0705cef67030ae0d
Seth Sharp <sesharp@outlook.com>
2024-03-15 13:46:32 +1000

    icons, file and new framed config for contact info and open times

---
+++ config/framed.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'name' => 'Framed Just For You',
+
+    'established' => 1997,
+
+    'contact' => [
+        'phone' => '0412 068 330',
+        'telephone' => '07 5563 9799',
+        'email' => 'framedjust4u@bigpond.com'
+    ],
+
+    'address' => 'Unit 1-2, 122 Brisbane Road, Labrador 4215',
+
+    'times' => [
+        [
+            'value' => 1,
+            'name' => 'Monday',
+            'open' => '8:30',
+            'close' => '5:00'
+        ],
+        [
+            'value' => 2,
+            'name' => 'Tuesday',
+            'open' => '8:30',
+            'close' => '5:00'
+        ],
+        [
+            'value' => 3,
+            'name' => 'Wednesday',
+            'open' => '8:30',
+            'close' => '5:00'
+        ],
+        [
+            'value' => 4,
+            'name' => 'Thursday',
+            'open' => '8:30',
+            'close' => '5:00'
+        ],
+        [
+            'value' => 5,
+            'name' => 'Friday',
+            'open' => '8:30',
+            'close' => '5:00'
+        ],
+        [
+            'value' => 6,
+            'name' => 'Saturday',
+            'open' => '9:00',
+            'close' => '4:00'
+        ]
+    ]
+];
--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,3 +1,35 @@
 <x-layouts.app>
-    Contact page
+    <x-section.section>
+        <x-section.wrapper>
+            {{ \Carbon\Carbon::now()->dayOfWeek  }}
+
+            <div class="bg-gradient-to-br from-white to-gray-200 rounded p-4">
+                <h1 class="text-4xl"> Contact Us Today </h1>
+
+                <div class="flex gap-4 mt-6">
+                    <div class="w-1/2">
+                        <div class="p-8 bg-white rounded text-secondary-500">
+                            <p class="text-secondary-400 text-md">
+                                We would love to know about your enquiries
+                            </p>
+
+                            <div class="mt-4">
+                                <h2 class="text-2xl"> Contact Information </h2>
+
+                                <ul>
+                                    <li class="flex">
+
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="w-1/2">
+                        <x-contact.map/>
+                    </div>
+                </div>
+            </div>
+        </x-section.wrapper>
+    </x-section.section>
 </x-layouts.app>
