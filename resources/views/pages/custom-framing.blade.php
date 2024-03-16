bb8ebbf7ef9ff22d99372b0dfe7bf9f05d0d8b32
Seth Sharp <sesharp@outlook.com>
2024-03-16 17:42:41 +1000

    wip

--- resources/views/pages/custom-framing.blade.php
+++ resources/views/pages/custom-framing.blade.php
@@ -1,3 +1,30 @@
 <x-layouts.app>
-    Custom Framing
+    <x-section.section>
+        <x-section.wrapper>
+            <x-section.content>
+                <x-slot name="title">
+                    Custom Picture Framing
+                </x-slot>
+
+                <x-slot name="content">
+                    At Framed Just For You, we specialize in custom framing that transforms your cherished memories into timeless works of art.
+                    Our expert craftsmen meticulously tailor each frame to suit your unique style and preserve the essence of your precious moments.
+                    Whether it's artwork, photographs, memorabilia, or more, we provide personalized framing solutions that elevate the beauty and significance
+                    of your keepsakes.
+                </x-slot>
+            </x-section.content>
+
+            <x-section.content>
+                <x-slot name="title">
+                    So what is custom framing exactly?
+                </x-slot>
+
+                <x-slot name="content">
+                    Most people when they think of custom framing, believe they do not need it and a cheap frame
+                    from kmart will suffice. Custom Framing is for those pieces of art work that deserve the best Australian
+                    Made materials and a frame that will be custom fit and designed to the art works needs.
+                </x-slot>
+            </x-section.content>
+        </x-section.wrapper>
+    </x-section.section>
 </x-layouts.app>
