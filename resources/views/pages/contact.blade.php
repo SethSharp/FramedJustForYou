1bc8dc0075e184d4f29888034571ba8e48bb7793
Seth Sharp <sesharp@outlook.com>
2024-03-17 15:32:30 +1000

    contact page responsiveness

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,25 +1,23 @@
 <x-layouts.app>
     <x-section.section>
         <x-section.wrapper>
-{{--            {{ \Carbon\Carbon::now()->dayOfWeek  }}--}}
-
             <div class="bg-gradient-to-br from-white to-gray-200 rounded p-4">
-                <div class="md:flex gap-4 p-8">
+                <div class="md:flex gap-4 space-y-4 sm:p-8">
                     <div class="md:w-1/2">
                         <div class="h-full">
                             <div>
                                 <x-headings.title> Get in touch </x-headings.title>
 
-                                <p class="text-secondary-400 font-medium text-md mt-6">
+                                <p class="text-secondary-400 font-medium text-sm sm:text-md mt-6">
                                     We would love to know about your enquiries
                                 </p>
 
                                 <ul class="mt-6 text-secondary-600 space-y-6">
-                                    <li class="flex gap-4">
+                                    <li class="flex gap-1 sm:gap-4">
                                         <x-icons.store-front/>
                                         <span class="my-auto"> {{ config('framed.address')  }} </span>
                                     </li>
-                                    <li class="flex gap-4">
+                                    <li class="flex gap-1 sm:gap-4">
                                         <div class="flex my-auto">
                                             <x-icons.telephone />
                                         </div>
@@ -27,7 +25,7 @@
                                             {{ config('framed.contact.telephone')  }}
                                         </span>
                                     </li>
-                                    <li class="flex gap-4">
+                                    <li class="flex gap-1 sm:gap-4">
                                         <div class="flex my-auto">
                                             <x-icons.mobile />
                                         </div>
@@ -35,13 +33,11 @@
                                             {{ config('framed.contact.mobile')  }}
                                         </span>
                                     </li>
-                                    <li class="flex gap-4">
+                                    <li class="flex gap-1 sm:gap-4">
                                         <div class="flex my-auto">
                                             <x-icons.envelope />
                                         </div>
-                                        <span class="my-auto">
-                                            {{ config('framed.contact.email')  }}
-                                        </span>
+                                        <span class="my-auto text-sm sm:text-base"> {{ config('framed.contact.email')  }} </span>
                                     </li>
                                 </ul>
                             </div>
@@ -68,7 +64,7 @@
                     </div>
                 </div>
 
-                <div class="m-6">
+                <div class="mt-4 sm:mt-0 sm:m-6">
                     <x-contact.map/>
                 </div>
             </div>
