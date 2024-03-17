a0b5ecd0f3c8c77c5b5fdf5f3eb1867acf118688
Seth Sharp <sesharp@outlook.com>
2024-03-17 11:05:56 +1000

    wip

--- resources/views/pages/printing.blade.php
+++ resources/views/pages/printing.blade.php
@@ -1,3 +1,7 @@
 <x-layouts.app>
-    Printing
+    <x-section.section>
+        <x-section.wrapper>
+            [Original stuff, just a reorganisation of what each pricing table is for.]
+        </x-section.wrapper>
+    </x-section.section>
 </x-layouts.app>
