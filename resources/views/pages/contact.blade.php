427b199daf9be51645e1b78d2eb004dafddf4df5
Seth Sharp <sesharp@outlook.com>
2024-03-23 07:20:20 +1000

    timezone

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -8,43 +8,41 @@
                 <div class="md:flex gap-4 space-y-4 sm:p-8">
                     <div class="md:w-1/2">
                         <div class="h-full">
-                            <div>
-                                <x-headings.title> Get in touch</x-headings.title>
+                            <x-headings.title> Get in touch</x-headings.title>
 
-                                <p class="text-secondary-400 font-medium text-sm sm:text-md mt-6">
-                                    We would love to know about your enquiries
-                                </p>
+                            <p class="text-secondary-400 font-medium text-sm sm:text-md mt-6">
+                                We would love to know about your enquiries
+                            </p>
 
-                                <ul class="mt-6 text-secondary-600 space-y-6">
-                                    <li class="flex gap-1 sm:gap-4">
-                                        <x-icons.store-front/>
-                                        <span class="my-auto"> {{ config('framed.address')  }} </span>
-                                    </li>
-                                    <li class="flex gap-1 sm:gap-4">
-                                        <div class="flex my-auto">
-                                            <x-icons.telephone/>
-                                        </div>
-                                        <span class="my-auto">
-                                            {{ config('framed.contact.telephone')  }}
-                                        </span>
-                                    </li>
-                                    <li class="flex gap-1 sm:gap-4">
-                                        <div class="flex my-auto">
-                                            <x-icons.mobile/>
-                                        </div>
-                                        <span class="my-auto">
-                                            {{ config('framed.contact.mobile')  }}
-                                        </span>
-                                    </li>
-                                    <li class="flex gap-1 sm:gap-4">
-                                        <div class="flex my-auto">
-                                            <x-icons.envelope/>
-                                        </div>
-                                        <span
-                                            class="my-auto text-sm sm:text-base"> {{ config('framed.contact.email')  }} </span>
-                                    </li>
-                                </ul>
-                            </div>
+                            <ul class="mt-6 text-secondary-600 space-y-6">
+                                <li class="flex gap-1 sm:gap-4">
+                                    <x-icons.store-front/>
+                                    <span class="my-auto"> {{ config('framed.address')  }} </span>
+                                </li>
+                                <li class="flex gap-1 sm:gap-4">
+                                    <div class="flex my-auto">
+                                        <x-icons.telephone/>
+                                    </div>
+                                    <span class="my-auto">
+                                        {{ config('framed.contact.telephone')  }}
+                                    </span>
+                                </li>
+                                <li class="flex gap-1 sm:gap-4">
+                                    <div class="flex my-auto">
+                                        <x-icons.mobile/>
+                                    </div>
+                                    <span class="my-auto">
+                                        {{ config('framed.contact.mobile')  }}
+                                    </span>
+                                </li>
+                                <li class="flex gap-1 sm:gap-4">
+                                    <div class="flex my-auto">
+                                        <x-icons.envelope/>
+                                    </div>
+                                    <span
+                                        class="my-auto text-sm sm:text-base"> {{ config('framed.contact.email')  }} </span>
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
