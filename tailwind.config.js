25e0a51ae697c38c988edc4729586a2a847dc085
Seth Sharp <sesharp@outlook.com>
2024-04-17 19:18:36 +1000

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
-        './resources/js/**/*.vue',
     ],
 
     safelist: [
