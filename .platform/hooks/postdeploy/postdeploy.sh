00547a560c5a1a51ad5695e59d70126ea8c0ebc4
Seth Sharp <sesharp@outlook.com>
2024-04-25 06:59:21 +1000

    attempt postdeploy

--- .platform/hooks/postdeploy/postdeploy.sh
+++ .platform/hooks/postdeploy/postdeploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 cd /var/www/html/ || { echo "Failed to change directory to /var/www/html/"; exit 1; }
+
+php artisan cache:clear
+php artisan config:cache
+php artisan view:cache
+php artisan route:cache
