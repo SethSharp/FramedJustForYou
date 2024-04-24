c45062adff9b62d50b83112bbd26f456d846e76f
Seth Sharp <sesharp@outlook.com>
2024-04-25 06:56:41 +1000

    Update predeploy.sh

--- .platform/hooks/predeploy/predeploy.sh
+++ .platform/hooks/predeploy/predeploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
 cd /var/www/html/ || { echo "Failed to change directory to /var/www/html/"; exit 1; }
-
-php artisan cache:clear
-php artisan config:clear
-php artisan view:clear
-php artisan route:clear
