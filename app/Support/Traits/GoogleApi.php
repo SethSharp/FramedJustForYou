7a4da08780ee7ba9dbcb25ccc7c6307650eb134d
Seth Sharp <sesharp@outlook.com>
2024-06-12 08:47:23 +1000

    Revert "temp fix"
    This reverts commit 53e1a3273aeecb48164298e8ae1098b9b3848060.

--- app/Support/Traits/GoogleApi.php
+++ app/Support/Traits/GoogleApi.php
@@ -10,9 +10,11 @@ trait GoogleApi
 {
     public function getRecentReviews(): Collection
     {
-        return collect($this->getLocalReviews()['reviews'])->sortByDesc('rating')->take(3)->values();
+        if (app()->environment('local')) {
+            return collect($this->getLocalReviews()['reviews'])->sortByDesc('rating')->take(3)->values();
+        }
 
-//        return cache()->remember(CacheKeys::googleReviews(), (24 * 60) * 7, fn () => collect($this->retrieveReviews())->sortByDesc('rating')->take(3)->values());
+        return cache()->remember(CacheKeys::googleReviews(), (24 * 60) * 7, fn () => collect($this->retrieveReviews())->sortByDesc('rating')->take(3)->values());
     }
 
     protected function getLocalReviews(): array
