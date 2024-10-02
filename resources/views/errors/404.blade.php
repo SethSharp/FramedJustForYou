d9519922d225e462cab5d174357073617f88c3df
Seth Sharp <sesharp@outlook.com>
2024-10-02 18:26:20 +1000

    improve 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,9 +1,13 @@
 <x-layouts.app>
-    <div class="p-24 min-h-screen">
-        <h1 class="text-8xl text-primary-500 animate-pulse duration-1000"> 404 </h1>
+    <div class="p-6 md:p-14 min-h-screen">
+        <h1 class="text-9xl text-primary-500 animate-pulse font-semibold font-mono duration-1000"> 404 </h1>
 
-        <p class="text-secondary-500">
+        <p class="text-secondary-500 my-2">
             Oops! Looks like we are still hanging this page up!
         </p>
+
+        <x-links.primary href="/">
+            Home
+        </x-links.primary>
     </div>
 </x-layouts.app>
