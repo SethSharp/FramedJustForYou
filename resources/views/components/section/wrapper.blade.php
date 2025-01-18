686819f8d896ffa967e930b193d93d3dec2c6d43
Seth Sharp <sesharp@outlook.com>
2025-01-19 08:24:36 +1000

    wip

--- resources/views/components/section/wrapper.blade.php
+++ resources/views/components/section/wrapper.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="transition-all duration-500 px-2 ease-in mx-10 py-6"
+    class="transition-all duration-500 px-2 ease-in mx-2 md:mx-10 py-6"
     x-data="{ inView: false }"
     x-intersect.threshold.05="inView = true"
     :class="{'translate-y-0 opacity-1 md:delay-75' : inView, 'translate-y-[2rem] opacity-0' : ! inView }"
