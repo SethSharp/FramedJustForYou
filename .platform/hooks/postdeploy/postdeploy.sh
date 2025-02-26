5d09ca57c348783817ac9b7de7f05f94ac15a567
Seth Sharp <58869086+SethSharp@users.noreply.github.com>
2025-02-26 10:58:53 +1000

    Delete .platform directory

--- .platform/hooks/postdeploy/postdeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /var/www/html/ || { echo "Failed to change directory to /var/www/html/"; exit 1; }
-
-php artisan cache:clear
-php artisan config:cache
-php artisan view:cache
-php artisan route:cache
